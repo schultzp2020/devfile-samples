161caeafcc6ca2b8bfc6d7ed626d364fe2761454
Paul Schultz <pschultz@pobox.com>
2021-07-08 16:57:13 -0500

    Fixed go

--- go/go.mod
+++ go/go.mod
@@ -1,1 +1,3 @@
-module golang.org/example
+module github.com/schultzp2020/devfile-starter-projects/go
+
+go 1.16
--- go/main.go
+++ go/main.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-    http.HandleFunc("/", HelloServer)
-    http.ListenAndServe("0.0.0.0:8080", nil)
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe("0.0.0.0:8080", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
